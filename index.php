7d2464744c2550b7090fa352ca7039bb01d4de41
Marina Gonzalez <marinagogu4@gmail.com>
2025-05-18 11:10:41 +0200

    Revert "INDEX: integrar buscador y refactorizar conexión a la base de datos"
    This reverts commit 1cb0b8305f6cbc1b0f509e26be003a7208268b06.

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
-<?php
-require_once('../aurea/functions/db.php');
-
-// Obtener productos destacados (máx 6)
-$productos = obtenerProductosDestacados(6);
-
-// Obtener 3 reseñas destacadas
-$resenas_result = obtenerResenasDestacadas(2);
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - Aurea</title>
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- CSS -->
+    <link rel="stylesheet" href="/aurea/assets/css/estilos.css">
     <link rel="stylesheet" href="/aurea/assets/css/estilos.css">
 </head>
 <body class="snap-container">
@@ -148,4 +139,9 @@ $resenas_result = obtenerResenasDestacadas(2);
     
     <?php include('partials/footer.php'); ?>  
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </html>
+
+
+
+
